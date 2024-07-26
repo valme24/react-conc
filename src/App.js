f34c3e1d29031c39e20894d9f4078cda875c2272
valme24 <vansh.goel.0403@gmail.com>
2024-07-26 12:21:31 +0530

    good css by mstk

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import Lst from './assets/lst';
+import React from 'react';
 function App() {
+  const fruts = [{d:1,name:'apple',calore:67},
+               {d:2,name:'banana',calore:819},
+               {d:3,name:'orange',calore:910},
+                {d:4,name:'grapes',calore:415},]
+const veg = [{d:1,name:'potato',calore:67},
+                {d:2,name:'banana',calore:819},
+                {d:3,name:'ono',calore:910},
+                 {d:4,name:'tomato',calore:415},];
   return (
    <>
-  <Lst></Lst>
+  <Lst items={fruts} catagory="fruts"> </Lst>
+  <Lst items={veg} catagory="vegetables"> </Lst>
+  
   </>
   );
 }
