fad52ae26b1239f2c053d48c4997e2483d1719b9
valme24 <vansh.goel.0403@gmail.com>
2024-07-29 18:22:56 +0530

    on change event handler'

--- src/my-component.jsx
+++ src/my-component.jsx
@@ -1,41 +1,55 @@
 import React,{useState} from 'react';
 function MyComponent() {
-    let [name, setname] = useState("guest");
-    let [age, setage] = useState(0);
-    let [isEmployee, setisEmployee] = useState(false);
-    const updateName = () => {
-        setname("spongebob");
-        console.log("function called");
-    }
-    const updateage = () => {
-        setage(age+1);
-        console.log("age incremented successfully");
-    }
-    const updateisEmployee = (e) => {
-        setisEmployee(!isEmployee);
-        if(isEmployee)
-            e.target.innerHTML="hire";
-        else 
-            e.target.innerHTML="fire";
-        
-        
-        
-    }
+    const [name,Setname]=useState("guest");
+    const [quantity,setquantity]=useState(1);
+    const [comment,setComment]=useState("");
+    const [payment,setpayment]=useState("");
+    const [shipping,setshipping]=useState("delivery");
 
+    function handleChange(event){
+        Setname(event.target.value);
+    }
+    function handleQunatityChange(event){
+        setquantity(event.target.value);
+    }
+    function handleCommentChange(event){
+        setComment(event.target.value);
+    }
+    function handlepaymentchange(event){
+        setpayment(event.target.value);
+    }   
+    function handleshippingchange(event){
+        setshipping(event.target.value);
+    }
     return (
        <>
-        <div>
-            <h1>name:{name}</h1>
-            <button onClick={()=>updateName()}>set name</button>
-        </div>
-        <div>
-            <h1>age:{age}</h1>
-            <button onClick={()=>updateage()}>increment age</button>
-        </div>
-        <div>
-            <h1>is isEmployee :{isEmployee?"yes":"no"} </h1>
-            <button onClick={(e)=>updateisEmployee(e)}>hire</button>
-        </div>
+<div>
+    <h1>{name}</h1>
+    <input value={name} onChange={handleChange} type="text"/>
+    <h1>{quantity}</h1>
+    <input value={quantity} onChange={handleQunatityChange} type="number"/>
+    <h3>delivery instructions</h3>
+    <input value={comment} onChange={handleCommentChange} type="text" placeholder='enter delivery instruction '/>
+    <p>{comment}</p>
+    <h3>payment methode selected :{payment} </h3>
+    <select value={payment} onChange={handlepaymentchange}>
+        <option value="cash">Cash</option>
+        <option value="credit card">credit card</option>
+        <option value="debit card">debit card</option>
+        <option value="upi">upi</option>
+        </select>
+        <h3>shipping methode {shipping}</h3>
+        <label>
+            <input type="radio" value="pick up" checked={shipping==="pick up"} onChange={handleshippingchange}/>
+             pick up
+        </label>
+        <br/>
+        <label >
+            <input type="radio" value="delivery" checked={shipping==="delivery"} onChange={handleshippingchange}/>
+            delivery
+        </label>
+        
+               </div>
        </>
     )
 }
