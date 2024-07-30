03e867c6448cc8467aaf113524b1fbfd898ea8c9
valme24 <vansh.goel.0403@gmail.com>
2024-07-30 22:48:39 +0530

    improeved app

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,36 +3,42 @@ import React,{useState} from 'react';
 
 function Todo(){
 const [tasks,SetTasks] = useState([]);
-const [task,setTask] = useState({title:"TITLE",
-                                 description:"DESCRIPTION",
+const [task,setTask] = useState({title:"",
+                                 description:"",
                                   date:new Date().getFullYear()});
-
+const [title,setTitle] = useState("");
+const [description,setDescription] = useState("");
+const [date,setDate] = useState(new Date());
 function addTask(){
-    SetTasks(c=>([...c,task]));
+    if (title !== "" && description !== "" && date !== "") {
+        const newTask = { title, description, date };
+        setTask(newTask);
+        
+        SetTasks(c => [...c, newTask]);
+        setTask({})
+        setTitle("");
+        setDescription("");
+        setDate(new Date());
+      }
 
 }
 function removeTask(index){
     SetTasks(tasks.filter((task,i)=>i!==index));
 }
-function handleTasksChange(event){
-    const temp = {title:task.title,description:task.description,date:task.date};
-    task.title="";
-    task.description="";
-    task.date="";
-}
+
 function handleDate(event){
-    setTask({...task,date:event.target.value});
-    event.target.value = new Date().getFullYear();
-    event.target.value=new Date().getFullYear();
+    setDate(event.target.value);
+  
+  // event.target.value=new Date();
 
 }
 function handleTitle(event){
-    setTask({...task,title:event.target.value});
-    event.target.value = "" 
+    setTitle(event.target.value);
+  //  event.target.value = "" ;
 }
 function handleDescription(event){
-    setTask({...task,description:event.target.value});
-    event.target.value = "" ;
+    setDescription(event.target.value);
+   // event.target.value = "" ;
     
 }
 
@@ -41,22 +47,22 @@ return(
     <div>
         <h1 id="heading">todo list</h1>
         
-        <input type="text" value={task.title} onChange={handleTitle} 
+        <input type="text" value={title} onChange={handleTitle} 
         placeholder='enter task title' className='input'/><br></br>
 
-        <input type="text" value={task.description} onChange={handleDescription}
+        <input type="text" value={description} onChange={handleDescription}
          placeholder='enter task description' className='input'/><br></br>
 
-        <input type="date" value={task.date} onChange={handleDate} 
+        <input type="date" value={date} onChange={handleDate} 
         placeholder='enter task date'className='input'/><br></br>
         <button onClick={addTask}>Add Task</button>
 
-        <ul> {tasks.map((tasks,index)=>
+        <ul> {tasks.map((taske,index)=>
         {
             return <li key={index} onClick={()=>removeTask(index)} 
-            className="task"><h3 className='task-title'>{tasks.title} </h3>
-            <p className='task-description'>{tasks.description}</p>
-            <p className='task-date'>{tasks.date}</p>
+            className="task"><h3 className='task-title'>{taske.title} </h3>
+            <p className='task-description'>{taske.description}</p>
+            <p className='task-date'>{taske.date}</p>
 
             </li>
         }
